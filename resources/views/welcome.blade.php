59a4cc48d554f64a07cfc16413eda6f62e16d641
PAULO VITOR COSTA DE SOUSA <202001434291@alunos.estacio.br>
2022-03-16 21:09:27 -0300

    vai q vai

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    <h1>Nivel de força dos ninjas de konoha: </h1>
+    <br><br>
+    <hr>
+
+    
+    <ol>
+        <li>1° hokage</li>
+        <li>2° hokage</li>
+        <li>3° hokage</li>
+        <li>4° hokage</li>
+        
+    </ol> 
+    <ol type="i">
+        <li>kakashi</li>
+        <li>Asuma</li>
+        <li>Kurenai</li>
+    </ol> 
+    <ol type="A">
+        <li>Iruka</li>
+        <li>Anko</li>
+        <li>Ibuso</li>
+    </ol>
+    <ol type="I">
+        <li>guy</li>
+        <li>itachi</li>
+        <li>shizuni</li>
+    </ol>
+
+
+    <ul type="disc" >
+        <li>kabuto</li>
+        <ul>
+            <li>zabuza</li>
+            <li>haku</li>
+
+        </ul>
+        <li>Rock lee</li>
+        <li>Sasuke</li>
+    </ul>
+    <ul type="circle">
+        <li>Shikamaru</li>
+        <li>Chochi</li>
+        <li>Naruto</li>
+
+    </ul>
+    <ul type="square">
+        <li>neji</li>
+        <li>Kiba</li>
+        <li>Akamaru</li>
+    </ul>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Tsunade</th>
+                <th>Orochimaru</th>
+                <th>Jiraya</th>
+                
+            </tr>
+         </thead>
+    </table>
+</body>
+</html>
